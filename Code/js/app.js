b75605cd4c4641491c742ae3ea6d6acbd2998edc
PiyushRavat <34954793+PiyushRavat@users.noreply.github.com>
2018-07-11 19:02:36 +0530

    added new project

---
+++ Code/js/app.js
@@ -0,0 +1,14 @@
+var resultDiv =  document.getElementById("result");
+resultDiv.innerHTML = "<p>here inject javascript</p>";
+
+/*var toggleButton = document.getElementById("toggleButton");
+toggleButton.on("click",function () {
+	console.log("button clicked");
+	resultDiv.toggle(500);	
+});*/
+
+$(document).ready(function(){
+	$('#toggleButton').click(function(){
+		$('#result').toggle(1000);
+	});
+});
