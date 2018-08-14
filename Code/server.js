2d4682134f7ebebc59e6f4f6977c146d75412874
Piyush Ravat <piyushravat7@gmail.com>
2018-08-14 15:43:19 +0530

    Update server.js

--- Code/server.js
+++ Code/server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var app = express();
-
-var port = 8090;
-
-app.use(express.static(__dirname + '/'));
-app.listen(port);
-
-console.log('Server started localhost at' +port);
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req,res){
+  //res.writehead(200,{'content-type : text/html'})
+ if(req.url =='/'){
+   fs.readFile('/index.html',utf-8,function(err,html){
+     res.writeHead(200,{'content-type :text/html'});
+     res.end(html);
+   });
+ }
+})
