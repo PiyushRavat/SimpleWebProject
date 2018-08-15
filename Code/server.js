98a82e13f07ddb1fa55d301e50b4470ec323d13a
Piyush Ravat <piyushravat7@gmail.com>
2018-08-15 22:08:02 +0530

    Update server.js

--- Code/server.js
+++ Code/server.js
@@ -9,3 +9,14 @@ http.createServer(function(req,res){
    });
  }
 })
+
+
+
+var express = require('express');
+var app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000,()=> {
+  console.log("server started at 3000");
+});
